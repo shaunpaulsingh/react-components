50cd4b8e10ae9f17530ad9e9fa0dd85b5484d367
Shaun Paul Singh <shaunpaulsingh@gmail.com>
2022-04-17 10:48:19 +0800

    First Commit

---
+++ src/components/Gallery.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+class Gallery extends Component{
+
+    render(){
+        return(
+            <div className="third left">
+                <h1>Gallery Component</h1>
+                <div className="gallery_item">
+                    <h3>Image Label</h3>
+                    <img className="gallery_img" src="./cat.jpg" />
+                </div>
+                <div className="gallery_item">
+                    <h3>Image Label</h3>
+                    <img className="gallery_img" src="./cat.jpg" />
+                </div>
+                <div className="gallery_item">
+                    <h3>Image Label</h3>
+                    <img className="gallery_img" src="./cat.jpg" />
+                </div>
+                <div className="gallery_item">
+                    <h3>Image Label</h3>
+                    <img className="gallery_img" src="./cat.jpg" />
+                </div>
+                <div className="gallery_item">
+                    <h3>Image Label</h3>
+                    <img className="gallery_img" src="./cat.jpg" />
+                </div>
+                <div className="gallery_item">
+                    <h3>Image Label</h3>
+                    <img className="gallery_img" src="./cat.jpg" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Gallery;
---
+++ src/components/Settings.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class Settings extends Component{
+
+    render(){
+        return(
+            <div className="third left">
+                <h1>Form Component</h1>
+                <form className="form_component">
+                    <label className="label_form">This is a text label</label>
+                    <input type="text" className="input_form"></input><br></br>
+                    <label className="label_form">This is a text label</label>
+                    <input type="textarea" className="input_form"></input><br></br>
+                    <label className="label_form">This is a text label</label>
+                    <input type="checkbox" className="input_form"></input><br></br>
+                    <button className="form_btn">This is a button.</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Settings;
